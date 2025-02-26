8b9629b752faa5f611e80b7007b782430d500060
shaheer shakir <shaheershakir22@gmail.com>
2025-02-26 22:41:44 +0300

    add logout functionality

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -9,7 +9,8 @@ urlpatterns = [
     # # path for registration
     # path for login
     path(route='login', view=views.login_user, name='login'),
-    
+    # path for logout
+    path(route='logout', view=views.logout_request, name='logout'),
 
     # path for dealer reviews view
 
