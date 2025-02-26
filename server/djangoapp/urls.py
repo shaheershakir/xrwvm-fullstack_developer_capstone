b810656470bc92bf5b3e591f0855f9fbd66cec78
shaheer shakir <shaheershakir22@gmail.com>
2025-02-26 23:12:33 +0300

    add registration

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -11,6 +11,8 @@ urlpatterns = [
     path(route='login', view=views.login_user, name='login'),
     # path for logout
     path(route='logout', view=views.logout_request, name='logout'),
+    # path for registration
+    path(route='register', view=views.registration, name='registration'),
 
     # path for dealer reviews view
 
