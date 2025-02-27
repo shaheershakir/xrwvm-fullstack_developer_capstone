6110d3a1250205747fcfafdc46ef1276a6055f80
shaheer shakir <shaheershakir22@gmail.com>
2025-02-27 23:38:39 +0300

    Add jshint version 8 to start of the file

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,3 +1,5 @@
+/*jshint esversion: 8 */
+
 import LoginPanel from "./components/Login/Login";
 import { Routes, Route } from "react-router-dom";
 import RegisterPanel from "./components/Register/Register";
