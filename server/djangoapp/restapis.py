1e7fb958e0651075e1ed88f4f04c57265f0df93f
shaheer shakir <shaheershakir22@gmail.com>
2025-02-27 22:26:08 +0300

    implement the react component dealers showing review

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -1,5 +1,5 @@
 # Uncomment the imports below before you add the function code
-# import requests
+import requests
 import os
 from dotenv import load_dotenv
 
@@ -26,6 +26,7 @@ def get_request(endpoint, **kwargs):
     except:
         # If any error occurs
         print("Network exception occurred")
+        return []
 
 
 def analyze_review_sentiments(text):
