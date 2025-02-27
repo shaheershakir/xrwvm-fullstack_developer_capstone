9a9e63d9348769a47a6bd667d635251375b85c5c
shaheer shakir <shaheershakir22@gmail.com>
2025-02-27 04:57:11 +0300

    build CarMake and CarModel Models

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -13,7 +13,8 @@ urlpatterns = [
     path(route='logout', view=views.logout_request, name='logout'),
     # path for registration
     path(route='register', view=views.registration, name='registration'),
-
+    # path for car list view
+    path(route='get_cars', view=views.get_cars, name ='getcars'),
     # path for dealer reviews view
 
     # path for add a review view
